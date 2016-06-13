0a6155668f4ab51074b4a5a04787910d8c559303
Brent Gardner <bgardner@squarelabs.net>
2016-06-12 23:31:57 -0600

    Implement more std methods

--- src/index.js
+++ src/index.js
@@ -5,6 +5,21 @@ const buffer = fs.readFileSync('/build/hello_world.wasm');
 var ffi = {
     env: {
         abort: function() {},
+        abortStackOverflow: function() {},
+        nullFunc_ii: function() {},
+		nullFunc_iiii: function() {},
+		nullFunc_vi: function() {},
+		_pthread_cleanup_pop: function() {},
+		___lock: function() {},
+		_abort: function() {},
+		_pthread_cleanup_push: function() {},
+		___syscall6: function() {},
+		_sbrk: function() {},
+		___syscall140: function() {},
+		_emscripten_memcpy_big: function() {},
+		___syscall54: function() {},
+		___unlock: function() {},
+		___syscall146: function() {},
         _foo: function() {return 42}
     }
 };
@@ -24,6 +39,11 @@ const ab = toArrayBuffer(buffer);
 const mod = Wasm.instantiateModule(ab, ffi);
 
 const res = mod.exports._main();
-console.log(res);
+console.log("res=", res);
+
+console.log(mod.exports.memory);
+
+const bytes = new Uint8Array(mod.exports.memory, res, 14);
+console.log(bytes);
 
 process.exit();
